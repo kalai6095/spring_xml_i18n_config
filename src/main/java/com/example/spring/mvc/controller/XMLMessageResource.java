0ba12a3165346f66893aa1b507d42c283e7aa8bd
Kalaiselvan A <kalai6095@gmail.com>
2019-09-11 11:56:03 +0000

    removed nullable annotation

--- src/main/java/com/example/spring/mvc/controller/XMLMessageResource.java
+++ src/main/java/com/example/spring/mvc/controller/XMLMessageResource.java
@@ -1,6 +1,6 @@
 package com.example.spring.mvc.controller;
 
-import jdk.internal.jline.internal.Nullable;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.context.MessageSource;
@@ -15,7 +15,7 @@ public class XMLMessageResource {
     @Autowired
     private MessageSource messageSource;
 
-    public String getMessage(String key, @Nullable Object[] arg, Locale locale) {
+    public String getMessage(String key, Object[] arg, Locale locale) {
         return messageSource.getMessage(key, arg, locale);
     }
 
